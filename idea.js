f99eb442125d47ca66126edbed00b1f768ad1fff
Devin Kapla <devinkapla@gmail.com>
2018-12-20 17:04:21 -0700

    Fix delete and persist

--- idea.js
+++ idea.js
@@ -1,15 +1,18 @@
 class Idea {
   constructor(id, title, body){
+    this.id = id;
     this.title = title;
     this.body = body;
     // this.quality = quality || swill;
-    this.id = id;
   }
-  saveToStorage(ideaArray) {
+
+  saveToStorage() {
     // create array - push array - then save array 
-    localStorage.setItem("ideaArray", JSON.stringify(ideaArray));
+    localStorage.setItem(this.id, JSON.stringify(this));
   }
+
   deleteFromStorage() {
     localStorage.removeItem(this.id);
   }
+
 }
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var titleInput = document.getElementById("title-input");
 var bodyInput = document.getElementById("body-input");
 var saveButton = document.querySelector(".save-button");
-var ideaArray = JSON.parse(localStorage.getItem("ideaArray")) || []; 
+// var ideaArray = JSON.parse(localStorage.getItem("ideaArray")) || []; 
+var ideaArray = []; 
 // var deleteButton = document.querySelector(".delete");
 
 saveButton.addEventListener("click", ideaClass);
@@ -9,55 +10,89 @@ window.addEventListener("load", cardPersist);
 // deleteButton.addEventListener("click", deleteCard);
 
 function cardPersist() {
-  ideaArray.forEach(function(e) {
-  newCard(e.id,e.title,e.body);
-  })
+  // forEach(function(e, idx) {
+    for(var i = 0; i < localStorage.length; i++){
+    console.log("initial for each entered")
+    var key = localStorage.key(i);
+    var item = JSON.parse(localStorage.getItem(key));
+    var newIdea = new Idea(item.id, item.title, item.body);
+    ideaArray.push(newIdea);
+    newCard(ideaArray[i]);
+   }
+  }
+  // })
+// }
+
+function ideaClass() {
+    var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
+    ideaArray.push(newIdea); 
+    newIdea.saveToStorage();
+    newCard(newIdea);
+    clearFields();
 }
 
-function newCard(id, title, body) {
+function newCard(idea) {
+
   var cardIdea = document.createElement("article")
   var newCard = document.querySelector(".card-section")
   cardIdea.className = 'new-card';
   cardIdea.innerHTML = 
-  `<article class="card">
-      <h2>${title}</h2>
-      <p>${body}</p>
+  `<article class="card" id="${idea.id}">
+      <h2>${idea.title}</h2>
+      <p>${idea.body}</p>
       <section class="button-corral">
         <div class="vote">
           <button class="downvote"></button>
           <button class="upvote"></button>
           <p class="quality">Quality</p>
         </div>
-        <button class="delete" data-id="${id}"></button>
+        <button class="delete" data-id="${idea.id}" onclick="deleteCard(${idea.id})"></button>
       </section>
     </article>`;
     newCard.insertBefore(cardIdea, newCard.firstChild);
-    var deleteButton = document.querySelector(".delete");
-    deleteButton.addEventListener("click", deleteCard);
+    // var deleteButton = document.querySelector(".delete");
+    // deleteButton.addEventListener("click", deleteCard);
   }
-    
-function ideaClass() {
-    var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
-    ideaArray.push(newIdea); 
-    newIdea.saveToStorage(ideaArray);
-    newCard(Date.now(), titleInput.value, bodyInput.value);
-    clearFields();
-}
 
 function clearFields() {
   titleInput.value = "";
   bodyInput.value = "";
 }
 
-function deleteCard(e) {
+function deleteCard(thisId) {
+  console.log(thisId)
   // let cardDelete = e.target.closest('.card').dataset.index;
-  if (e.target.className === 'delete') {
-    e.target.parentElement.parentElement.parentElement.remove();
-    let cardId = this.getAttribute('data-id');
-    console.log(cardId);
-    let cardDelete = ideaArray.find(x => x.id == cardId);
-    console.log(cardDelete);
+  // if (e.target.className === 'delete') {
+    // e.target.parentElement.parentElement.parentElement.remove();
+
+    // let cardId = this.getAttribute('data-id');
+
+
+
+    let cardDelete = ideaArray.find(x => x.id == thisId);
+    
+
+    let index = ideaArray.findIndex(x => x.id == thisId);
+    
+    ideaArray.splice(index,1);
+    // var cardId = (thisId).toString();
+    // console.log(cardId);
+    var wholeCard = document.getElementById(thisId.toString());
+    wholeCard.remove();
+
     cardDelete.deleteFromStorage();
+
+    // console.log(cardId);
+    // console.log(index);
+    // console.log(ideaArray);
+    // console.log(oldIdea);
+
+    // ideaArray.forEach(function(e){
+    //   if (e.id == cardId) {
+    //     console.log(e)
+    //     e.deleteFromStorage()
+    //   }
+    // })
     // let id = e.target.closest('.card').dataset.index;
 
 
@@ -71,7 +106,7 @@ function deleteCard(e) {
     //   }
     // })
     // cardId.deleteFromStorage();
-  }
+  
 }
 
 //     document.querySelector(".delete").addEventListener("click", function(){
