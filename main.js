92eb7df522ddfb411ad00aa1f32b68ff1bbed0cb
Devin Kapla <devinkapla@gmail.com>
2018-12-22 21:29:26 -0700

    Add content editable functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var titleInput = document.getElementById("title-input");
 var bodyInput = document.getElementById("body-input");
 var saveButton = document.querySelector(".save-button");
+var cardSection = document.querySelector(".card-section");
 var ideaArray = []; 
 
 saveButton.addEventListener("click", ideaClass);
@@ -8,41 +9,47 @@ window.addEventListener("load", cardPersist);
 
 function cardPersist() {
   Object.keys(localStorage).forEach(function(card){
-  var item = JSON.parse(localStorage.getItem(card)); 
-  var newIdea = new Idea(item.id, item.title, item.body, item.quality);  
-  ideaArray.push(newIdea);
-  newCard(newIdea);
+    var item = JSON.parse(localStorage.getItem(card)); 
+    var newIdea = new Idea(item.id, item.title, item.body, item.quality);  
+    ideaArray.push(newIdea);
+    newCard(newIdea);
   });
-  }
+}
 
 function ideaClass() {
-    var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
-    ideaArray.push(newIdea); 
-    newIdea.saveToStorage();
-    newCard(newIdea);
-    clearFields();
+  var newIdea = new Idea(Date.now(), titleInput.value, bodyInput.value);
+  ideaArray.push(newIdea); 
+  newIdea.saveToStorage();
+  newCard(newIdea);
+  clearFields();
 }
 
 function newCard(idea) {
-  var newCard = document.querySelector(".card-section");
-  newCard.insertAdjacentHTML('afterbegin',
-  `<article class="card" id="${idea.id}">
-      <h2 contenteditable="true">${idea.title}</h2>
-      <p contenteditable="true">${idea.body}</p>
-      <section class="button-corral">
-        <div class="vote">
-          <img class="downvote" onclick="qualityChangeDown(${idea.id})" src="images/downvote.svg">
-          <img class="upvote" onclick="qualityChangeUp(${idea.id})" src="images/upvote.svg">
-          <p class="quality">Quality: ${idea.quality}</p>
-        </div>
-        <img class="delete" data-id="${idea.id}" onclick="deleteCard(${idea.id})" src="images/delete.svg">
-      </section>
+  cardSection.insertAdjacentHTML('afterbegin',
+    `<article class="card"  id="${idea.id}">
+    <h2 class="title" data-id="${idea.id}" contenteditable="true" onfocusout="cardChange('title')" onkeydown="enterKey('title')">${idea.title}</h2>
+    <p class="body" data-id="${idea.id}" contenteditable="true" onfocusout="cardChange('body')" onkeydown="enterKey('body')">${idea.body}</p>
+    <section class="button-corral">
+    <div class="vote">
+    <img class="downvote" onclick="qualityChangeDown(${idea.id})" src="images/downvote.svg">
+    <img class="upvote" onclick="qualityChangeUp(${idea.id})" src="images/upvote.svg">
+    <p class="quality">Quality: ${idea.quality}</p>
+    </div>
+    <img class="delete" data-id="${idea.id}" onclick="deleteCard(${idea.id})" src="images/delete.svg">
+    </section>
     </article>`);
-  }
+}
 
 function clearFields() {
   titleInput.value = "";
   bodyInput.value = "";
+}
+
+function enterKey (category) {
+  var key = event.keyCode;
+  if (key === 13) { 
+    cardChange(category);
+  }
 }
 
 function deleteCard(thisId) {
@@ -80,6 +87,22 @@ function qualityChangeUp(cardId) {
   if (quality === "plausible") {
     card.updateQuality();
   }
-
 }
+
+
+function cardChange(category) {
+  if (category === 'title') {
+    let cardId = JSON.parse(event.target.dataset.id);
+    let card = ideaArray.find(idea => idea.id === cardId);
+    let newText = event.target.innerText;
+    card.updateContent(newText, category);
+  }
+  if (category === 'body') {
+    let cardId = JSON.parse(event.target.dataset.id);
+    let card = ideaArray.find(idea => idea.id === cardId);
+    let newText = event.target.innerText;
+    card.updateContent(newText, category);
+  }
+}
+
 
